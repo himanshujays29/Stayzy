71945789514396d15cecf5a2e8287eb0097fc9cb
Himanshujays29 <himanshujayswal2006@gmail.com>
2025-07-13 22:53:19 +0530

    Update & Delete Route Added

--- index.js
+++ index.js
@@ -3,6 +3,7 @@ import mongoose from 'mongoose';
 import Listing from "./models/listing.js";
 import path from 'path';
 import { fileURLToPath } from 'url';
+import methodOverride from 'method-override';
 
 const app = express();
 const port = 8080;
@@ -12,6 +13,7 @@ const __dirname = path.dirname(__filename);
 app.set("views", path.join(__dirname, "views"));
 app.set("view engine", "ejs");
 app.use(express.urlencoded({extended: true}));
+app.use(methodOverride("_method"));
 
 
 const MONGO_URL = "mongodb://127.0.0.1:27017/Stayzy";
@@ -57,6 +59,13 @@ app.get("/listings", async (req, res) => {
    res.render("listings/index.ejs", {allListings});
 });
 
+//New Route
+
+app.get("/listings/new", (req, res ) => {
+    res.render("listings/new.ejs");
+})
+
+
 //Show Route 
 
 app.get ("/listings/:id", async (req, res) => {
@@ -65,6 +74,42 @@ app.get ("/listings/:id", async (req, res) => {
     res.render("listings/show.ejs", {listing});
 });
 
+//Create Route
+
+app.post("/listings",async (req, res) => {
+   const newListing =  new Listing(req.body.listing);
+   await newListing.save();
+   res.redirect("/listings");
+});
+
+// Edit route 
+
+app.get("/listings/:id/edit", async (req, res) => {
+    let {id} = req.params;
+    const listing = await Listing.findById(id); 
+    res.render("listings/edit.ejs", {listing});
+});
+
+//Update Route
+
+app.put("/listings/:id",async (req, res) => {
+    let {id} = req.params;
+    await Listing.findByIdAndUpdate(id, { ...req.body.listing });
+    res.redirect(`/listings/${id}`);
+});
+
+//Delete Route 
+
+app.delete("/listings/:id", async (req, res) =>{
+    let {id} = req.params;
+    let deletedListing = await Listing.findByIdAndDelete(id);
+    console.log(deletedListing);
+    res.redirect("/listings")
+});
+
+
 app.listen(port, ()=>{
     console.log(`Server is listning to port ${port}`); 
 });
+
+
