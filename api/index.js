34b102cfae596083e41456c1a40a1d02467e7709
Himanshujays29 <himanshujayswal2006@gmail.com>
2026-01-08 17:28:23 +0530

    final commit

--- api/index.js
+++ api/index.js
@@ -4,73 +4,79 @@ import path from "path";
 import { fileURLToPath } from "url";
 import methodOverride from "method-override";
 import ejsMate from "ejs-mate";
-import ExpressError from "./utils/ExpressError.js";
+import ExpressError from "../utils/ExpressError.js";
 import session from "express-session";
-import MongoStore from 'connect-mongo'
+import MongoStore from "connect-mongo";
 import flash from "connect-flash";
 import passport from "passport";
 import localStrategy from "passport-local";
-import User from "./models/user.js";
+import User from "../models/user.js";
 import fileUpload from "express-fileupload";
 
-import listingsRouter from "./routes/listing.js";
-import reviewsRouter from "./routes/review.js";
-import userRouter from "./routes/user.js";
+import listingsRouter from "../routes/listing.js";
+import reviewsRouter from "../routes/review.js";
+import userRouter from "../routes/user.js";
 
 const app = express();
-const port = 8080;
 
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
-app.set("views", path.join(__dirname, "views"));
+
+// Views
+app.set("views", path.join(__dirname, "../views"));
 app.set("view engine", "ejs");
+app.engine("ejs", ejsMate);
+
+// Middleware
 app.use(express.urlencoded({ extended: true }));
 app.use(methodOverride("_method"));
-app.engine("ejs", ejsMate);
-app.use(express.static(path.join(__dirname, "/public")));
+app.use(express.static(path.join(__dirname, "../public")));
 app.use(fileUpload({
   useTempFiles: true,
   tempFileDir: "/tmp/"
 }));
 
-const dbUrl= process.env.ATLASDB_URL;
+// Database
+const dbUrl = process.env.ATLASDB_URL;
+let isConnected = false;
 
+async function main() {
+  if (isConnected) return;
+  await mongoose.connect(dbUrl);
+  isConnected = true;
+  console.log("MongoDB connected");
+}
+main().catch(console.error);
+
+// Session store
 const store = MongoStore.create({
   mongoUrl: dbUrl,
-  crypto: {
-    secret: process.env.SESSION_SECRET,
-  },
+  crypto: { secret: process.env.SESSION_SECRET },
   touchAfter: 24 * 3600,
 });
 
-store.on("error", function (e) {
-  console.log("SESSION STORE ERROR", e);
-});
-
 const sessionOptions = {
-  store: store,
+  store,
   secret: process.env.SESSION_SECRET,
   resave: false,
   saveUninitialized: true,
-  Cookie: {
-    expires: Date.now() + 7 * 24 * 60 * 60 * 1000,
+  cookie: {
     maxAge: 7 * 24 * 60 * 60 * 1000,
     httpOnly: true,
   },
 };
 
-
 app.use(session(sessionOptions));
 app.use(flash());
 
-// Passport Athuntication
-
+// Passport
 app.use(passport.initialize());
 app.use(passport.session());
 passport.use(new localStrategy(User.authenticate()));
 passport.serializeUser(User.serializeUser());
 passport.deserializeUser(User.deserializeUser());
 
+// Locals
 app.use((req, res, next) => {
   res.locals.success = req.flash("success");
   res.locals.error = req.flash("error");
@@ -78,6 +84,7 @@ app.use((req, res, next) => {
   next();
 });
 
+// Routes
 app.get("/", (req, res) => {
   res.render("listings/home.ejs");
 });
@@ -90,32 +97,19 @@ app.get("/termsConditions", (req, res) => {
   res.render("pptc/termsConditions.ejs");
 });
 
-main()
-  .then((res) => {
-    console.log("Connected to DB");
-  })
-  .catch((err) => {
-    console.log(err);
-  });
-
-async function main() {
-  await mongoose.connect(dbUrl);
-}
-
 app.use("/listings", listingsRouter);
 app.use("/listings/:id/reviews", reviewsRouter);
 app.use("/", userRouter);
 
-// Error Handling Middleware
-app.all("/{*any}", (req, res, next) => {
+// Errors
+app.all("*", (req, res, next) => {
   next(new ExpressError(404, "Page not found!"));
 });
 
 app.use((err, req, res, next) => {
-  let { statusCode = 500, message = "Somthing Went Wrong!" } = err;
+  let { statusCode = 500, message = "Something went wrong!" } = err;
   res.status(statusCode).render("listings/error.ejs", { message });
 });
 
-app.listen(port, () => {
-  console.log(`Server is listning to port ${port}`);
-});
+// ✅ Export (NO listen)
+export default app;
